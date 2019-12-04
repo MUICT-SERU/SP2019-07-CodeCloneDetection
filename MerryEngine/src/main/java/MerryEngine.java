c74eba70a339af88696a18ee99ae0bfe695ce098
sidekoiii <sidekoiii@MacBook-Pro-khxng-sidekoiii.local>
2019-12-04 16:22:32 +0700

    read file fix

--- MerryEngine/src/main/java/MerryEngine.java
+++ MerryEngine/src/main/java/MerryEngine.java
@@ -8,7 +8,7 @@ public class MerryEngine {
         System.out.println("running...");
         System.out.println("Input : " + cmd.getInputSource());
 
-        final File folder = new File("/home/sidekoiii/"+ cmd.getInputSource());
+        final File folder = new File(cmd.getInputSource());
         List<String> fileList = new ArrayList<String>();
         search(".*\\.java", folder, fileList);
         List<Method> methodList = new ArrayList<Method>();
@@ -26,20 +26,21 @@ public class MerryEngine {
 //        for(Method m : methodList){
 //            System.out.println(m.getSourceCode());
 //        }
-        Method exampleMethod = methodList.get(100);
-        System.out.println("Method 1 : "+ exampleMethod.getMethodName());
-        System.out.println(exampleMethod.getFilePath()+"   "+exampleMethod.getStartLine()+"   "+exampleMethod.getEndLine());
-        System.out.println(exampleMethod.getSourceCode());
-        System.out.println("Method list size : "+methodList.size());
-        System.out.println("Number of token : "+exampleMethod.getTokenNo()+" with unique : "+exampleMethod.getUniqueTokenNo());
-        System.out.println("Number of Identifier : "+exampleMethod.getIdentifierNo()+" with unique : "+exampleMethod.getUniqueIdentifierNo());
-        System.out.println("Number of Operator: "+exampleMethod.getOperatorNo()+" with unique : "+exampleMethod.getUniqueOperatorNo());
-        System.out.println("Return Type : "+exampleMethod.getReturnType());
+//        Method exampleMethod = methodList.get(10);
+//        System.out.println("Method 1 : "+ exampleMethod.getMethodName());
+//        System.out.println(exampleMethod.getFilePath()+"   "+exampleMethod.getStartLine()+"   "+exampleMethod.getEndLine());
+//        System.out.println(exampleMethod.getSourceCode());
+//        System.out.println("Method list size : "+methodList.size());
+//        System.out.println("Number of token : "+exampleMethod.getTokenNo()+" with unique : "+exampleMethod.getUniqueTokenNo());
+//        System.out.println("Number of Identifier : "+exampleMethod.getIdentifierNo()+" with unique : "+exampleMethod.getUniqueIdentifierNo());
+//        System.out.println("Number of Operator: "+exampleMethod.getOperatorNo()+" with unique : "+exampleMethod.getUniqueOperatorNo());
+//        System.out.println("Return Type : "+exampleMethod.getReturnType());
         List<MethodPair> methodPairList = new ArrayList<MethodPair>();
         for(int i = 0 ; i < methodList.size();i++){
             for(int j=i+1 ; j<methodList.size();j++){
                 MethodPair methodPair = new MethodPair(methodList.get(i),methodList.get(j));
                 methodPairList.add(methodPair);
+//                System.out.println((i*methodList.size())+j);
             }
         }
         System.out.println("Method Pair List size : " + methodPairList.size());
