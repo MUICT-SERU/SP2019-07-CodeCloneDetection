220dbeada347c716d53041b94a7013eb91c05d76
sidekoiii <france29368@gmail.com>
2020-01-11 15:04:26 +0700

    add call code2vec Python

--- MerryEngine/src/main/java/Method.java
+++ MerryEngine/src/main/java/Method.java
@@ -1,5 +1,7 @@
 import org.antlr.v4.runtime.Token;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
@@ -24,6 +26,7 @@ public class Method {
     private int uniqueOperatorNo;
     private int tokenTypeDiversity;
     private String returnType;
+    private String code2vecVector;
 
 
     public Method(int start, int end, String path,String fileName, String code, String name,String type) throws Exception {
@@ -37,6 +40,8 @@ public class Method {
         this.returnType = type;
         gatherMetrics();
         writeFile(this.sourceCode);
+        this.code2vecVector = executeCode2Vec();
+        System.out.println("code2vec Vector : \n"+this.code2vecVector);
     }
 
     private void gatherMetrics() throws Exception {
@@ -81,15 +86,62 @@ public class Method {
 
     private void writeFile(String code){
         try {
-            FileWriter myWriter = new FileWriter("/Users/sidekoiii/Documents/code2vec/Input.java");
+            FileWriter myWriter = new FileWriter("Input.java");
             myWriter.write(code);
             myWriter.close();
-//            System.out.println("Successfully wrote to the file.");
+            System.out.println("Successfully wrote to the file.");
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
 
+    }
+
+    private String executeCode2Vec() throws IOException {
+        // set up the command and parameter
+//        String pythonScriptPath = "code2vec.py";
+//        String args = "--load models/java14_model/saved_model_iter8.release --predict --export_code_vectors";
+        boolean readVector = false;
+        String vector = "";
+        String s = null;
+        try {
+
+            // run the Unix "ps -ef" command
+            // using the Runtime exec method:/
+            Process p = Runtime.getRuntime().exec("python3 SP2019-DoNotCopy/MerryEngine/code2vec/code2vec.py --load SP2019-DoNotCopy/MerryEngine/code2vec/models/java14_model/saved_model_iter8.release --predict --export_code_vectors");
+
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(p.getInputStream()));
+
+            BufferedReader stdError = new BufferedReader(new
+                    InputStreamReader(p.getErrorStream()));
+
+            // read the output from the command
+            System.out.println("Here is the standard output of the command:\n");
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+                if(readVector == true){
+                    vector += s.toString();
+                }
+                if(s.contains("Code vector")){
+                    readVector = true;
+                }
+            }
+
+            // read any errors from the attempted command
+            System.out.println("Here is the standard error of the command (if any):\n");
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+
+//            System.exit(0);
+        }
+        catch (IOException e) {
+            System.out.println("exception happened - here's what I know: ");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        return vector;
     }
 
     public int getStartLine() {
