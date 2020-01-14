6423c0d777d6c7207ffeb54759911f30c57377f0
weekitaus <46718771+weekitaus@users.noreply.github.com>
2020-01-14 14:23:12 +0700

    fix some bugs

--- nodeJS_Rest/app.js
+++ nodeJS_Rest/app.js
@@ -2,26 +2,21 @@ const express = require('express')
 // Import the axios library, to make HTTP requests
 const axios = require('axios')
 
-<<<<<<< Updated upstream
-=======
+var bodyParser = require('body-parser')
+
 const { exec, execFile } = require('child_process');
->>>>>>> Stashed changes
+
 // const exec = require('child_process')
 
 const config = require('./config/config.js');
 
 const app = express()
 
-<<<<<<< Updated upstream
-const clientID = '31ec50555fca84725afa'
-const clientSecret = 'a3d38c40372c265b0942450d0ebf1c1d1dad54b1'
-=======
 app.use(bodyParser.json({ type: 'application/json' }))
 
 const clientID = `${global.gConfig.clientID}`;
 const clientSecret = `${global.gConfig.clientSecret}`;
 
->>>>>>> Stashed changes
 // Declare the redirect route
 app.get('/oauth/redirect', (req, res) => {
   // The req.query object has the query params that
@@ -49,29 +44,14 @@ app.get('/oauth/redirect', (req, res) => {
 
 app.use(express.static(__dirname + '/frontend'))
 
-<<<<<<< Updated upstream
-// app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.post('/urlcloning',(req,res) => {
-//   console.log(`${req.body.GitHubURL}`);
-//   exec('git clone '+`${req.body.GitHubURL}`+' ./test',(error, stdout, stderr) => {
-//     if (error) {
-//     console.error(`exec error: ${error}`);
-//     return;
-//   }
-//   console.log(`stdout: ${stdout}`);
-//   console.error(`stderr: ${stderr}`);
-//   res.send(alert(`stderr: ${stderr}`));
-//   })
-// })
-=======
-app.post('/api/clone', async function(req, res) {
+app.post('/api/clone', function(req, res) {
   // console.log(req.params.url)
   console.log(req.body.github)
 
   res.json(req.body)
 
-  await exec('git clone '+`${req.body.github}`+' ./temp', (error, stdout, stderr) => {
+  exec('git clone '+`${req.body.github}`+' ./temp', (error, stdout, stderr) => {
     if (error) {
         console.error(`exec error: ${error}`);
         return;
@@ -80,17 +60,16 @@ app.post('/api/clone', async function(req, res) {
       console.error(`stderr: ${stderr}`);
 
     })
->>>>>>> Stashed changes
 
-    await exec('java -jar simian-2.5.10.jar -reportDuplicateText ./temp/*.java',(error, stdout, stderr) => {
-      if (error) {
-          console.error(`exec error: ${error}`);
-          return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.error(`stderr: ${stderr}`);
 })
-})
+// , exec('java -jar simian-2.5.10.jar -reportDuplicateText ./temp/*.java',(error, stdout, stderr) => {
+//   if (error) {
+//       console.error(`exec error: ${error}`);
+//       return;
+//     }
+//     console.log(`stdout: ${stdout}`);
+//     console.error(`stderr: ${stderr}`);
+// })
 //localhost 8001
 app.listen(global.gConfig.node_port,() => {
   console.log(`${global.gConfig.app_name} listening on port ${global.gConfig.node_port}`);
